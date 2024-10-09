67a11112a44908e6b902a151beef593bbad72b47
Gaarge <vladram3707@gmail.com>
2024-10-09 20:29:11 +0300

    sec

--- go/main.go
+++ go/main.go
@@ -1,24 +1,61 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
-func main() {
-	mainDir := http.Dir("../static")
-	staticHandler := http.FileServer(mainDir)
-	http.Handle("/", staticHandler)
+type cssDate struct {
+	CSS string
+}
 
-	stylesDir := http.Dir("../styles")
-	stylesHandler := http.FileServer(stylesDir)
-	staticHandler = http.StripPrefix("/styles/", stylesHandler)
-	http.Handle("/styles/", staticHandler)
+func renderTemplate(w http.ResponseWriter, tmpl string, data cssDate) {
+	templates, err := template.ParseFiles(
+		"../templates/base.html",
+		"../templates/header.html",
+		"../templates/footer.html",
+		"../templates/"+tmpl+".html",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = templates.ExecuteTemplate(w, "base.html", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index", cssDate{CSS: "index.css"})
+}
+
+func main() {
+
+	http.HandleFunc("/", index)
+
+	staticDir := http.Dir("../static")
+	staticHandler := http.FileServer(staticDir)
+	staticHandler = http.StripPrefix("/static/", staticHandler)
+	http.Handle("/static/", staticHandler)
 
 	/*
-		picturesDir := http.Dir("../pictures")
-		picturesHandler := http.FileServer(picturesDir)
-		picturesHandler = http.StripPrefix("/pictures/", picturesHandler)
-		http.Handle("/pictures/", picturesHandler)
+
+		mainDir := http.Dir("../templates")
+		templatesHandler := http.FileServer(mainDir)
+		http.Handle("/", templatesHandler)
+
+		staticDir := http.Dir("../static")
+		staticHandler := http.FileServer(staticDir)
+		templatesHandler = http.StripPrefix("/static/", staticHandler)
+		http.Handle("/static/", templatesHandler)
+
+
+			picturesDir := http.Dir("../pictures")
+			picturesHandler := http.FileServer(picturesDir)
+			picturesHandler = http.StripPrefix("/pictures/", picturesHandler)
+			http.Handle("/pictures/", picturesHandler)
 	*/
 	http.ListenAndServe(":8080", nil)
 }
