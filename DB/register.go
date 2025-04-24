7d7ef77eff54352e19930021b37a31eb7124a314
Gaarge <gaarge1142#gmail.com>
2025-04-24 22:54:37 +0300

    fin

--- DB/register.go
+++ DB/register.go
@@ -37,12 +37,12 @@ func login(w http.ResponseWriter, r *http.Request) {
 
 		if err != nil {
 			if err == sql.ErrNoRows {
-				http.Redirect(w, r, "http://static-site:8080/bad_pass", http.StatusSeeOther)
+				http.Redirect(w, r, "http://stat:8080/bad_pass", http.StatusSeeOther)
 			}
 			return
 		}
 
-		http.Redirect(w, r, "http://static-site:8080/lkp", http.StatusSeeOther)
+		http.Redirect(w, r, "http://stat:8080/lkp", http.StatusSeeOther)
 	}
 }
 
@@ -65,7 +65,7 @@ func register(w http.ResponseWriter, r *http.Request) {
 		}
 
 		log.Println("Регистрация успешна")
-		http.Redirect(w, r, "http://static-site:8080/lkp", http.StatusSeeOther)
+		http.Redirect(w, r, "http://static:8080/lkp", http.StatusSeeOther)
 	}
 }
 
